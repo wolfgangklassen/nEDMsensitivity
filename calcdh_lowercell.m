f4ea801a594d38849f5679225b7110285f8d4966
Wolfgang Klassen <wolfyklassen@gmail.com>
2019-01-08 16:27:24 -0600

    commit

--- calcdh_lowercell.m
+++ calcdh_lowercell.m
@@ -1,5 +1,5 @@
 function dh_lowercell = calcdh_lowercell(par)
 
-dh_lowercell = (-par.d_EDMguide/2)-(3/2)*(par.h_elec/2) - par.h_EDMcell - par.d_cellguide;
+dh_lowercell = (-par.d_EDMguide/2)-(3/2)*(par.h_elec) - par.h_EDMcell - par.d_cellguide;
 
 end
