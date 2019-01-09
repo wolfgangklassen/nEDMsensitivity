c44a937a73883a825a0f176c3f682f0c5b5fa16e
Wolfgang Klassen <wolfyklassen@gmail.com>
2019-01-09 12:57:03 -0600

    more debugging against the spreadsheet

--- calcVol_guidevapour.m
+++ calcVol_guidevapour.m
@@ -1,5 +1,5 @@
 function Vol_guidevapour = calcVol_guidevapour(par)
 
-Vol_guidevapour = (par.L_guidevapour*pi*(par.d_guidesource^2)*1e6)/(4);
+Vol_guidevapour = (par.L_guidevapour*pi*(par.d_guidevapour^2)*1e6)/(4);
 
 end
--- calcVol_prod.m
+++ calcVol_prod.m
@@ -1,5 +1,5 @@
 function Vol_prod = calcVol_prod(par)
 
-Vol_prod = (par.L_prod*pi*(par.d_guidesource^2)*1e6)/(4);
+Vol_prod = (par.L_prod*pi*(par.d_prod^2)*1e6)/(4);
 
 end
