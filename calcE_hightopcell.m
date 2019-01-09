5130e5214e61140b26a069cd1f7b8ac287d439c5
Wolfgang Klassen <wolfyklassen@gmail.com>
2019-01-09 13:49:13 -0600

    working version

--- calcE_hightopcell.m
+++ calcE_hightopcell.m
@@ -1,5 +1,5 @@
 function E_hightopcell = calcE_hightopcell(const,par,E_highsourceexit,h_uppercell)
 
-E_hightopcell = min(E_highsourceexit,(par.V_EDMcell + h_uppercell*const.V_ngrav));
+E_hightopcell = min(E_highsourceexit,par.V_EDMcell + (h_uppercell*const.V_ngrav));
 
 end
--- main.m
+++ main.m
@@ -15,7 +15,7 @@ Vol_EDMguide = calcVol_EDMguide(par);%good, cm^3
 
 Vol_EDMcell = calcVol_EDMcell(par);%good, cm^3
 
-h_Hecont = calch_Hecont(par);%sheet says .40, might be wrong, m
+h_Hecont = calch_Hecont(par);%good, m
 
 h_guidesource = calch_guidesource(par);%ok, m
 
@@ -31,74 +31,74 @@ T_3He = calcT_3He(const);%good, s
 
 T_phon = calcT_phon(par);%good, s
 
-T_vapour = calcT_vapour(par);
+T_vapour = calcT_vapour(par);%good, s
 
-Vol_guidevapour = calcVol_guidevapour(par);
+Vol_guidevapour = calcVol_guidevapour(par);%needs looking at, answer is 1000 cm^3 high
 
-Vol_guideLHe = calcVol_guideLHe(par);
+Vol_guideLHe = calcVol_guideLHe(par);%ok, still a little low, cm^3
 
-Vol_prod = calcVol_prod(par);
+Vol_prod = calcVol_prod(par);%low, this formula assumes a cylindrical production volume, cm^3
 
-alpha = calcalpha(par);
+alpha = calcalpha(par);%good
 
-NDens_Hg = calcNDens_Hg(const,par);
+NDens_Hg = calcNDens_Hg(const,par);%good, m^-3
 
-Vel_Hg = calcVel_Hg(const,par);
+Vel_Hg = calcVel_Hg(const,par);%good, m/s
 
 %%below here order matters%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-MFP_UCN = calcMFP_UCN(const,Vel_Hg,NDens_Hg);%
+MFP_UCN = calcMFP_UCN(const,Vel_Hg,NDens_Hg);%good, m
 
-T_HgAbs = calcT_HgAbs(MFP_UCN,Vel_Hg);
+T_HgAbs = calcT_HgAbs(MFP_UCN,Vel_Hg);%good, s
 
-h_EDMguide = calch_EDMguide(h_guidesource,h_Hecont);
+h_EDMguide = calch_EDMguide(h_guidesource,h_Hecont);%good, m
 
-h_maxuppercell = calch_maxuppercell(h_EDMguide,dh_uppercell);
+h_maxuppercell = calch_maxuppercell(h_EDMguide,dh_uppercell);%good, m
 
-h_uppercell = calch_uppercell(par,h_guidesource,h_Hecont);
+h_uppercell = calch_uppercell(par,h_guidesource,h_Hecont);%good, m
 
-E_lowafterirrad = calcE_lowafterirrad(E_lowprod);
+E_lowafterirrad = calcE_lowafterirrad(E_lowprod);%good, neV
 
-E_lowsourceexit = calcE_lowsourceexit(const,E_lowafterirrad,h_guidesource,h_Hecont);
+E_lowsourceexit = calcE_lowsourceexit(const,E_lowafterirrad,h_guidesource,h_Hecont);%close, formula needs updating, neV
 
-E_lowtopcell = calcE_lowtopcell(const,E_lowsourceexit,h_maxuppercell,h_uppercell);
+E_lowtopcell = calcE_lowtopcell(const,E_lowsourceexit,h_maxuppercell,h_uppercell);%good, neV
 
-E_highsourceexit = calcE_highsourceexit(E_highafterirrad);
+E_highsourceexit = calcE_highsourceexit(E_highafterirrad);%good, neV
 
-E_hightopcell = calcE_hightopcell(const,par,E_highsourceexit,h_uppercell);
+E_hightopcell = calcE_hightopcell(const,par,E_highsourceexit,h_uppercell);%good
 
-F_uppercell = calcF_uppercell(par,E_hightopcell,E_lowtopcell,E_highsourceexit,E_lowsourceexit);
+F_uppercell = calcF_uppercell(par,E_hightopcell,E_lowtopcell,E_highsourceexit,E_lowsourceexit);%close
 
-h_bottomcell = calch_bottomcell(par,h_EDMguide);
+h_bottomcell = calch_bottomcell(par,h_EDMguide);%good
 
-E_lowbottomcell = calcE_lowbottomcell(const,E_lowsourceexit,h_bottomcell);
+E_lowbottomcell = calcE_lowbottomcell(const,E_lowsourceexit,h_bottomcell);%close, neV
 
-h_minlowercell = calch_minlowercell(h_EDMguide,dh_lowercell);
+h_minlowercell = calch_minlowercell(h_EDMguide,dh_lowercell);%good, m
 
-E_highbottomcell = calcE_highbottomcell(const,par,E_highsourceexit,V_EDMguide,h_minlowercell,h_bottomcell);
+E_highbottomcell = calcE_highbottomcell(const,par,E_highsourceexit,V_EDMguide,h_minlowercell,h_bottomcell);%good
 
-F_lowercell = calcF_lowercell(par,E_highbottomcell,E_lowbottomcell,E_highsourceexit,E_lowsourceexit);
+F_lowercell = calcF_lowercell(par,E_highbottomcell,E_lowbottomcell,E_highsourceexit,E_lowsourceexit);%good
 
-F_EDMcell = calcF_EDMcell(par,Vol_EDMcell,Vol_EDMguide,Vol_prod,Vol_guideLHe,Vol_guidevapour);
+F_EDMcell = calcF_EDMcell(par,Vol_EDMcell,Vol_EDMguide,Vol_prod,Vol_guideLHe,Vol_guidevapour);%good
 
-Eff_cellfill = calcEff_cellfill(par,F_EDMcell,F_lowercell,F_uppercell);
+Eff_cellfill = calcEff_cellfill(par,F_EDMcell,F_lowercell,F_uppercell);%good
 
-F_sourceexit = calcF_sourceexit(par,E_highsourceexit,E_lowsourceexit,E_highafterirrad,E_lowafterirrad);
+F_sourceexit = calcF_sourceexit(par,E_highsourceexit,E_lowsourceexit,E_highafterirrad,E_lowafterirrad);%good
 
-F_source = calcF_source(par,E_highafterirrad,E_lowafterirrad,E_highprod,E_lowprod);
+F_source = calcF_source(par,E_highafterirrad,E_lowafterirrad,E_highprod,E_lowprod);%good
 
-VolFrac_He = calcVolFrac_He(Vol_prod,Vol_guideLHe,Vol_guidevapour);
+VolFrac_He = calcVolFrac_He(Vol_prod,Vol_guideLHe,Vol_guidevapour);%probably good
 
-T_source = calcT_source(par,VolFrac_He,T_vapour,T_phon,T_3He);
+T_source = calcT_source(par,VolFrac_He,T_vapour,T_phon,T_3He);%close, low, s
 
-T_irrad = calcT_irrad(par,T_source);
+T_irrad = calcT_irrad(par,T_source);%close, low because Tsource is low, s
 
-N_afterirrad = calcN_afterirrad(par,T_source,T_irrad,F_source);
+N_afterirrad = calcN_afterirrad(par,T_source,T_irrad,F_source);%close, low
 
-N_UCNt0 = calcN_UCNt0(N_afterirrad,F_sourceexit,Eff_cellfill);
+N_UCNt0 = calcN_UCNt0(N_afterirrad,F_sourceexit,Eff_cellfill);%close, low
 
-N_after = calcN_after(par,N_UCNt0,T_HgAbs);
+N_after = calcN_after(par,N_UCNt0,T_HgAbs);%close, low
 
-N_gooducndetected = calcN_gooducndetected(par,N_after);
+N_gooducndetected = calcN_gooducndetected(par,N_after);%close, low
 
 S_perfill = calcS_perfill(const,par,T_ramsey,N_gooducndetected,alpha)
